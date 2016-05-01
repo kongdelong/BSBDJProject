2db10e706839bf5f59f1962e53bd0cc645daa344
komgdelong <3124172@qq.com>
2016-05-02 01:23:31 +0800

    项目框加搭建 － 设置导航栏

--- 百思不得姐(KDL)/百思不得姐(KDL)/UIView+XLExtension.h
+++ 百思不得姐(KDL)/百思不得姐(KDL)/UIView+XLExtension.h
@@ -10,6 +10,7 @@
 
 @interface UIView (XLExtension)
 
+@property (nonatomic, assign) CGSize size;
 @property (nonatomic, assign) CGFloat width;
 @property (nonatomic, assign) CGFloat height;
 @property (nonatomic, assign) CGFloat x;
--- 百思不得姐(KDL)/百思不得姐(KDL)/XLEssenceViewController.m
+++ 百思不得姐(KDL)/百思不得姐(KDL)/XLEssenceViewController.m
@@ -16,22 +16,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // 设置导航栏标题
+    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"MainTitle"]];
+    // 设置导航栏左边的按钮
+    UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [leftButton setBackgroundImage:[UIImage imageNamed:@"MainTagSubIcon"] forState:UIControlStateNormal];
+    [leftButton setBackgroundImage:[UIImage imageNamed:@"MainTagSubIconClick"] forState:UIControlStateHighlighted];
+    leftButton.size =  leftButton.currentBackgroundImage.size;
+    [leftButton addTarget:self action:@selector(leftBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftButton];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)leftBtnClick
+{
+    XLLogFunc;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
