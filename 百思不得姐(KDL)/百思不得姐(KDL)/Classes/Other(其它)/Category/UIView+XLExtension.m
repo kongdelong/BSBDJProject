11824eeda7f5d7114051ddf4e027f9d9ec4df91f
komgdelong <3124172@qq.com>
2016-05-17 21:33:05 +0800

    增加标签的选中

--- 百思不得姐(KDL)/百思不得姐(KDL)/Classes/Other(其它)/Category/UIView+XLExtension.m
+++ 百思不得姐(KDL)/百思不得姐(KDL)/Classes/Other(其它)/Category/UIView+XLExtension.m
@@ -51,6 +51,30 @@
     self.frame = frame;
 }
 
+- (void)setCenterX:(CGFloat)centerX
+{
+    CGPoint center = self.center;
+    center.x = centerX;
+    self.center = center;
+}
+
+- (void)setCenterY:(CGFloat)centerY
+{
+    CGPoint center = self.center;
+    center.y = centerY;
+    self.center = center;
+}
+
+- (CGFloat)centerX
+{
+    return  self.center.x;
+}
+
+- (CGFloat)centerY
+{
+    return  self.center.y;
+}
+
 - (CGFloat)width
 {
     return self.frame.size.width;
