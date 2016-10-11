63f289e1a10e5c616f8306c8e2f96eb76dad67be
kongdelong <wcb@CbdeMacBook-Pro.local>
2016-10-11 10:49:59 +0800

    xcode8 mjrefresh  崩溃解决

---
+++ 百思不得姐(KDL)/百思不得姐(KDL)/Classes/Other(其它)/Category/XLHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  XLHomeViewController.h
+//  miaowPlay
+//
+//  Created by Cb W on 16/8/15.
+//  Copyright © 2016年 kdl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XLHomeViewController : UIViewController
+
+@end
